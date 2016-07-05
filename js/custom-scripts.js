54598286dee04d426f81f90708029b4c9d777f89
jkipker <jkipker@gmail.com>
2016-07-05 18:27:35 -0500

    basic siding change functionality in place

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
 	//AL STEEL BUILD YOUR OWN METAL BUILDING SCRIPT
+
+	var colorToChange = "siding";
+
+
+
+	//** swap metal siding **//
+	$( ".color-choice" ).click(function() {
+  		 var myColor = event.target.id;
+  		 var sidingURL = "img/siding/";
+  		 var roofURL = "img/roof/";
+  		 var trimURL = "img/trim/";  		 
+
+
+  		 if(colorToChange == "siding"){
+  		 	$('#siding').attr("src", sidingURL+myColor+'.png');
+  		 } else if(colorToChange == "siding"){
+  		 	$('#roof').attr("src", sidingURL+myColor+'.png');
+  		 } else if(colorToChange == "siding"){
+  		 	$('#trim').attr("src", sidingURL+myColor+'.png');
+  		 }
+  		 
+  		
+  		 
+
+	});
+
 });
