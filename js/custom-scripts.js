cf520be1b4912d649d6ccb32b4d9a739b2b653ea
jkipker <jkipker@gmail.com>
2016-07-06 12:43:31 -0500

    color selections & layout complete

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -15,15 +15,36 @@ $(document).ready(function(){
 
   		 if(colorToChange == "siding"){
   		 	$('#siding').attr("src", sidingURL+myColor+'.png');
-  		 } else if(colorToChange == "siding"){
-  		 	$('#roof').attr("src", sidingURL+myColor+'.png');
-  		 } else if(colorToChange == "siding"){
-  		 	$('#trim').attr("src", sidingURL+myColor+'.png');
+  		 	console.log('sidingURL = '+sidingURL+' '+myColor);
+  		 } else if(colorToChange == "roof"){
+  		 	$('#roof').attr("src", roofURL+myColor+'.png');
+  		 	console.log('roofURL = '+roofURL+' '+myColor);
+  		 } else if(colorToChange == "trim"){
+  		 	$('#trim').attr("src", trimURL+myColor+'.png');
+  		 	console.log('trimURL = '+trimURL+' '+myColor);
   		 }
-  		 
-  		
-  		 
+	});
 
+	$( "#siding-tab" ).click(function() {
+		$( ".siding-tab" ).addClass("selected");
+		$( ".roof-tab" ).removeClass("selected");
+		$( ".trim-tab" ).removeClass("selected");
+
+		colorToChange = "siding";
+	});
+	$( "#roof-tab" ).click(function() {
+		$( ".siding-tab" ).removeClass("selected");
+		$( ".roof-tab" ).addClass("selected");
+		$( ".trim-tab" ).removeClass("selected");
+
+		colorToChange = "roof";
+	});
+	$( "#trim-tab" ).click(function() {
+		$( ".siding-tab" ).removeClass("selected");
+		$( ".roof-tab" ).removeClass("selected");
+		$( ".trim-tab" ).addClass("selected");
+
+		colorToChange = "trim";
 	});
 
 });
